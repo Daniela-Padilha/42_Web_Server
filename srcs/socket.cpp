7b2e3986133b429d0b8b5bcfa220a050a4620fd7
Daniela-Padilha <danielasofiapadilha@gmail.com>
2026-01-27 17:19:42 +0000

    refactor: clarified var names and comments

--- srcs/socket.cpp
+++ srcs/socket.cpp
@@ -1,12 +1,12 @@
 #include "../inc/webserv.hpp"
 
 int socket_creator(void) {
-	int socket_fd;
+	int server_fd;
 
-	// create socket
-	socket_fd = socket(AF_INET, SOCK_STREAM, 0); //AF_INET (address family for IPv4),
+	// create server socket
+	server_fd = socket(AF_INET, SOCK_STREAM, 0); //AF_INET (address family for IPv4),
 												//SOCK_STREAM (socket type using TCP/IP)
-	if (socket_fd < 0) {
+	if (server_fd < 0) {
 		strerror(errno);
 		return 1;
 	}
@@ -14,36 +14,36 @@ int socket_creator(void) {
 	// create struct with local address info
 	sockaddr_in addr;
 	addr.sin_family = AF_INET; //address family
-	addr.sin_port = htons(8080); //port nbr in network byte order
 	addr.sin_addr.s_addr = INADDR_ANY; //IP address (0.0.0.0)
+	addr.sin_port = htons(8080); //port nbr in network byte order
 
-	// choose address + port that socket will use
-	if (bind(socket_fd, (sockaddr*)&addr, sizeof(addr)) < 0) {
+	// choose address + port that server will use
+	if (bind(server_fd, (sockaddr*)&addr, sizeof(addr)) < 0) {
 		strerror(errno);
-		close(socket_fd);
+		close(server_fd);
 		return 1;
 	}
 
-	// get socket ready for incoming connection attempts
-	if (listen(socket_fd, 10) < 0) {
+	// get server ready for incoming connection attempts
+	if (listen(server_fd, 10) < 0) {
 		strerror(errno);
-		close(socket_fd);
+		close(server_fd);
 		return 1;
 	}
 
 	std::cout << "Server is listening on port 8080...\n";
 
-	// create client
+	// client info
 	sockaddr_in client_addr;
 	int client_fd;
 	socklen_t client_len;
 
 	// waits a connection, when it arrives, opens a new socket to communicate
 	client_len = sizeof(client_addr);
-	client_fd = accept(socket_fd, (sockaddr*)&client_addr, &client_len);
+	client_fd = accept(server_fd, (sockaddr*)&client_addr, &client_len);
 	if (client_fd < 0) {
 		strerror(errno);
-		close(socket_fd);
+		close(server_fd);
 		return 1;
 	}
 
@@ -51,6 +51,6 @@ int socket_creator(void) {
 
 	// cleanup
 	close(client_fd);
-	close(socket_fd);
+	close(server_fd);
 	return 0;
 }
