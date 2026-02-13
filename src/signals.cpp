06d79a106c7f8d031fbe67ffbe7888a2eec5e01e
gris <185072671+d-branco@users.noreply.github.com>
2026-02-13 18:29:47 +0000

    feat: signals closing gracefully

---
+++ src/signals.cpp
@@ -0,0 +1,20 @@
+#include "../inc/signals.hpp"
+
+static void handle_signals(int sig)
+{
+	(void) sig;
+	std::cout << "  Signal received. Exiting...\n";
+	g_looping = 0;
+}
+
+void init_signals()
+{
+	struct sigaction s_a;
+	s_a.sa_handler = handle_signals;
+	s_a.sa_flags   = 0;
+	sigemptyset(&s_a.sa_mask);
+
+	sigaction(SIGINT, &s_a, NULL);
+	sigaction(SIGQUIT, &s_a, NULL);
+	sigaction(SIGTERM, &s_a, NULL);
+}
