f1d80f15af3dde683afd6745714722fe382056fb
Ricardo Garcia <costagarcia.r95@gmail.com>
2026-03-02 00:36:18 +0000

    feat(cgi): add execute function to execute script

--- inc/CGI.hpp
+++ inc/CGI.hpp
@@ -4,17 +4,28 @@
 # include <vector>
 # include <string>
 # include <unistd.h>
+# include <sys/wait.h>
+# include <signal.h>
+
 # include "HTTPRequest.hpp"
+# include "Config.hpp"
 
 class CGI {
 	private:
 		std::vector<std::string> _env;
+		
 		void	_setEnvironment();
+		std::string _getScriptPath() const;
+		void	_handleTimeout(pid_t pid);
 
+		// The HTTP request that triggered the CGI execution, used to set environment variables and determine the script path
 		const HTTPRequest& _request;
+		
+		// The route configuration that matched the request, used to determine the script path and other settings
+		const RouteConfig& _route;
 
 	public:
-		CGI();
+		CGI(const HTTPRequest& request, const RouteConfig& route);
 		CGI(const CGI& other);
 		CGI& operator=(const CGI& other);
 		~CGI();
--- src/CGI.cpp
+++ src/CGI.cpp
@@ -1,6 +1,7 @@
 #include "../inc/CGI.hpp"
 
-CGI::CGI(){}
+CGI::CGI(const HTTPRequest& request, const RouteConfig& route)
+    : _request(request), _route(route) {}
 
 CGI::CGI(const CGI& other) {
     *this = other;
@@ -17,42 +18,62 @@ CGI::~CGI(){}
 
 // Check RFC 3875 for CGI environment variables: https://datatracker.ietf.org/doc/html/rfc3875
 void CGI::_setEnvironment() {
-    // Query string is the part of the URL after the '?' character, if any
-    std::string query_string = "";
     std::string request_target = _request.get_request_target();
-    size_t query_pos = request_target.find('?');
+
+    std::string script_name = request_target; // Setting it to the full request target as a default value, in case there is no query string
+    std::string query_string = "";
+    size_t  query_pos = request_target.find('?');
     if (query_pos != std::string::npos) {
+        script_name = request_target.substr(0, query_pos);
         query_string = request_target.substr(query_pos + 1);
     }
-
-    // Method used by the script to process the request
-    _env.push_back("REQUEST_METHOD=" + _request.get_method());
-
-    // The MIME type of the request body, if any
-    _env.push_back("CONTENT_TYPE=" + _request.get_header_value("content-type"));
     
-    // The length of the request body, if any
+    // RFC 3875 required CGI environment variables
+    _env.push_back("GATEWAY_INTERFACE=CGI/1.1");
+    _env.push_back("SERVER_PROTOCOL=" + _request.get_protocol());
+    _env.push_back("SERVER_NAME=" + _request.get_header_value("host"));
+    _env.push_back("REQUEST_METHOD=" + _request.get_method());
+    _env.push_back("SCRIPT_NAME=" + script_name);
+    _env.push_back("PATH_INFO=" + request_target);
+    _env.push_back("QUERY_STRING=" + query_string);
+    _env.push_back("CONTENT_TYPE=" + _request.get_header_value("content-type"));
     _env.push_back("CONTENT_LENGTH=" + _request.get_header_value("content-length"));
     
-    // The query string, if any
-    _env.push_back("QUERY_STRING=" + query_string);
-    
-    //_env.push_back("SCRIPT_NAME=" + ...); the URL parh as the client requested it
-    
-    // The path to the resource being requested, relative to the document root
-    _env.push_back("PATH_INFO=" + request_target);
-
-    // The protocol version used by the client
-    _env.push_back("SERVER_PROTOCOL=" + _request.get_protocol());
-
-    // The server's hostname or IP address
-    _env.push_back("SERVER_NAME=" + _request.get_header_value("host"));
-
-    // php-cgi specifically requires it as a security measure
+    // php-cgi specific
+    _env.push_back("SCRIPT_FILENAME=" + _getScriptPath());
     _env.push_back("REDIRECT_STATUS=200");
 }
 
+std::string CGI::_getScriptPath() const {
+    std::string request_target = _request.get_request_target();
+
+    size_t  query_pos = request_target.find('?');
+    if (query_pos != std::string::npos) {
+        request_target = request_target.substr(0, query_pos);
+    }
+    return _route.root + request_target;
+}
+
+void    CGI::_handleTimeout(pid_t pid) {
+    int status;
+    int elapsed = 0;
+
+    while (elapsed < 5) {
+        if (waitpid(pid, &status, WNOHANG) == pid) {
+            return;
+        }
+        sleep(1);
+        elapsed++;
+    }
+
+    // 5 seconds have passed and the child process is still running, kill it
+    kill(pid, SIGKILL);         // force kill the script
+    waitpid(pid, &status, 0);   // clean up zombie process
+}
+
 std::string CGI::execute() {
+    _setEnvironment();
+
     int stdin_pipe[2];
     int stdout_pipe[2];
 
@@ -68,10 +89,47 @@ std::string CGI::execute() {
         close(stdin_pipe[1]);
         close(stdout_pipe[0]);
 
-        //...
+        std::vector<char*> env_ptrs;
+        for (size_t i = 0; i < _env.size(); ++i) {
+            env_ptrs.push_back(const_cast<char*>(_env[i].c_str()));
+        }
+        env_ptrs.push_back(NULL);
+
+        std::string script_path = _getScriptPath();
+        std::string script_dir = script_path.substr(0, script_path.find_last_of('/'));
+        chdir(script_dir.c_str());
+
+        char* argv[] = {
+            const_cast<char*>(_route.cgi_path.c_str()),
+            const_cast<char*>(script_path.c_str()),
+            NULL
+        };
+
+        execve(_route.cgi_path.c_str(), argv, &env_ptrs[0]);
+        exit(1);
     }
     else {
         close(stdin_pipe[0]);
         close(stdout_pipe[1]);
+
+        // write POST body to child stdin
+        if (_request.get_method() == "POST") {
+            const std::string& body = _request.get_body();
+            write(stdin_pipe[1], body.c_str(), body.size());
+        }
+        close(stdin_pipe[1]); // signal EOF to child
+
+        std::string output;
+        char buffer[1024];
+        ssize_t bytes_read;
+        while ((bytes_read = read(stdout_pipe[0], buffer, sizeof(buffer))) > 0) {
+            output.append(buffer, bytes_read);
+        }
+        close(stdout_pipe[0]);
+
+        // wait for child process to finish, with a timeout to prevent hanging
+        _handleTimeout(pid);
+
+        return output;
     }
 }
